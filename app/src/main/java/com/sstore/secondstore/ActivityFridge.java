36782080095e3ba562a97d38017670ea54bdba4b
JJHW <otl9882@naver.com>
2018-12-28 02:53:50 +0900

    초기작업

---
+++ app/src/main/java/com/sstore/secondstore/ActivityFridge.java
@@ -0,0 +1,13 @@
+package com.sstore.secondstore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityFridge extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fridge);
+    }
+}
