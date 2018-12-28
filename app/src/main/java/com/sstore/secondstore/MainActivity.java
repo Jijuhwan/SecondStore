5d53a7a5fa42552b0b9930776484226198ab325a
Jijuhwan <jijuhuan@gmail.com>
2018-12-28 19:03:22 +0900

    UI 재작업 / 네이버 카페로 이동 완료

--- app/src/main/java/com/sstore/secondstore/MainActivity.java
+++ app/src/main/java/com/sstore/secondstore/MainActivity.java
@@ -1,6 +1,7 @@
 package com.sstore.secondstore;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
     public void Fridge(View v)
     {
         Intent intent = new Intent(this, ActivityFridge.class);
+        startActivity(intent);
+    }
+    public void llNaver(View v)
+    {
+        String url ="https://cafe.naver.com/jjm9892";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
         startActivity(intent);
     }
 }
